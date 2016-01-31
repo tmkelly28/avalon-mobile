51fe006e7183f03230c37c8270f126a838894f75
Thomas Kelly <tmkelly28@gmail.com>
2016-01-31 07:22:24 -0500

    Moved functions to room view controller

--- www/js/room/room.js
+++ www/js/room/room.js
@@ -22,22 +22,7 @@ app.config($stateProvider => {
 	});
 });
 
-app.controller('RoomCtrl', ($scope, game, chats, user, players, userRecord, Session, FbChatService, FbGamesService, FirebaseEvents) => {
-
-    function _remove (playerArray, player) {
-        let idx = _.findIndex(playerArray, { _id: player._id });
-        idx > -1 ? playerArray.splice(idx, 1) : null;
-    }
-
-    function _has (playerArray, player) {
-        return _.findIndex(playerArray, { _id: player._id }) > -1;
-    }
-
-    function _notFirstTeamProposalOfQuest () {
-        return game.currentGamePhase === 'team building' && game.currentQuestIdx !== 0 && game.currentVoteTrack !== 0;
-    }
-
-	const addToTeam = (player) => FbGamesService.addToTeam($scope.game.$id, player);
+app.controller('RoomCtrl', ($scope, game, chats, user, players, userRecord, RoomViewCtrl, FirebaseEvents) => {
 
     $scope.game = game;
     $scope.chats = chats;
@@ -50,77 +35,30 @@ app.controller('RoomCtrl', ($scope, game, chats, user, players, userRecord, Sess
     $scope.gameStartModal = $scope.ladyModal = $scope.questResultModal = null;
 
     FirebaseEvents.registerListeners($scope.game, $scope.userRecord, $scope);
+    RoomViewCtrl.registerScope($scope);
 
-    $scope.closeGameStartModal = () => $scope.gameStartModal.hide().then(() => $scope.gameStartModal.remove());
-    $scope.closeLadyModal = () => $scope.ladyModal.hide().then(() => $scope.ladyModal.remove());
-    $scope.closeQuestResultModal = () => $scope.questResultModal.hide().then(() => $scope.questResultModal.remove());
-
-    $scope.addMessage = (message) => message.text ? FbChatService.addChat($scope.chats, Session.user, message.text) : null;
-    $scope.isHost = () => Session.user._id === $scope.game.host;
-    $scope.ableToBegin = () => Object.keys($scope.game.players).length >= $scope.game.targetSize && Object.keys($scope.game.players).length < 11;
-    $scope.startGame = () => FbGamesService.startGame($scope.game);
-    $scope.me = (player) => player._id === $scope.user._id;
-	$scope.proposeTeam = () => {
-        return Promise.all($scope.selected.map(playerId => addToTeam(playerId)))
-            .then(() => {
-                $scope.selected = [];
-                FbGamesService.proposeTeam($scope.game.$id);
-            });
-    };
-	$scope.disablePropose = () => $scope.selected.length !== $scope.game.currentQuestPlayersNeeded;
-    $scope.resetTeam = () => {
-        $scope.selected = [];
-        FbGamesService.resetTeam($scope.game.$id)
-    };
-    $scope.voteApprove = () => FbGamesService.approveTeam($scope.game.$id, $scope.userRecord.playerKey);
-    $scope.voteReject = () => FbGamesService.rejectTeam($scope.game.$id, $scope.userRecord.playerKey);
-    $scope.successQuest = () => FbGamesService.voteToSucceed($scope.game.$id, $scope.userRecord.playerKey);
-    $scope.failQuest = () => FbGamesService.voteToFail($scope.game.$id, $scope.userRecord.playerKey);
-    $scope.guessMerlin = () => {
-        let _player = $scope.selected[0];
-        $scope.selected = [];
-        FbGamesService.guessMerlin($scope.game.$id, _player);
-    };
-	$scope.range = (n, m) => _.range(n, m);
-    $scope.disableLady = () => {
-        if ($scope.selected.length !== 1) return true;
-        if ($scope.selected.length === 1) {
-            let _player = $scope.selected[0];
-            if (_player.hasBeenLadyOfTheLake || _player._id === user._id) return true;
-        }
-        return false;
-    };
-    $scope.disableGuessMerlin = () => {
-        if ($scope.selected.length !== 1) return true;
-        if ($scope.selected.length === 1) {
-            let _player = $scope.selected[0];
-            if (_player.loyalty !== 'good' || (_player.loyalty === 'evil' && _player.character !== 'Oberon')) return true;
-        }
-        return false;
-    };
-	$scope.useLady = () => {
-        let _player = $scope.selected[0];
-        $scope.selected = [];
-		$scope.investigatedPlayer = {
-			loyalty: _player.loyalty,
-			displayName: _player.displayName
-		}
-		FbGamesService.useLady($scope.game.$id, _player, $scope);
-	};
-    $scope.isSelected = (player) => _has($scope.selected, player);
-    $scope.select = (player) => $scope.isSelected(player) ? _remove($scope.selected, player) : $scope.selected.push(player);
-    $scope.revealPicture = (player) => {
-        if ($scope.game.waitingToPlay) return player.picture;
-        if (player.onQuest && $scope.game.currentGamePhase === 'team voting') return '/img/sigil.png';
-        if (player.approvedQuest && ($scope.game.currentGamePhase === 'quest voting' || _notFirstTeamProposalOfQuest())) return '/img/approve.png';
-        if (!player.approvedQuest && $scope.game.currentGamePhase === 'quest voting' || _notFirstTeamProposalOfQuest()) return '/img/reject.png';
-        if (game.useLady && game.currentLadyOfTheLake._id === player._id && $scope.game.currentGamePhase.slice(0, 3) !== 'end') return '/img/lady_of_the_lake.png';
-        if ($scope.me(player) ||
-            ($scope.game.currentGamePhase.slice(0, 3) === 'end') ||
-            ($scope.game.currentGamePhase === 'guess merlin' &&
-                player.loyalty === 'evil' &&
-                player.character !== 'Oberon')) return player.imageUrl;
-        return player.picture;
-    }
+    $scope.closeGameStartModal = RoomViewCtrl.closeGameStartModal;
+    $scope.closeLadyModal = RoomViewCtrl.closeLadyModal;
+    $scope.closeQuestResultModal = RoomViewCtrl.closeQuestResultModal;
+    $scope.addMessage = RoomViewCtrl.addMessage;
+    $scope.isHost = RoomViewCtrl.isHost;
+    $scope.ableToBegin = RoomViewCtrl.ableToBegin;
+    $scope.startGame = RoomViewCtrl.startGame;
+    $scope.me = RoomViewCtrl.me;
+	$scope.proposeTeam = RoomViewCtrl.proposeTeam;
+	$scope.disablePropose = RoomViewCtrl.disablePropose;
+    $scope.resetTeam = RoomViewCtrl.resetTeam;
+    $scope.voteApprove = RoomViewCtrl.voteApprove;
+    $scope.voteReject = RoomViewCtrl.voteReject;
+    $scope.successQuest = RoomViewCtrl.successQuest;
+    $scope.failQuest = RoomViewCtrl.failQuest;
+    $scope.guessMerlin = RoomViewCtrl.guessMerlin;
+	$scope.range = RoomViewCtrl.range;
+    $scope.disableLady = RoomViewCtrl.disableLady;
+    $scope.disableGuessMerlin = RoomViewCtrl.disableGuessMerlin;
+	$scope.useLady = RoomViewCtrl.useLady;
+    $scope.isSelected = RoomViewCtrl.isSelected;
+    $scope.select = RoomViewCtrl.select;
+    $scope.revealPicture = RoomViewCtrl.revealPicture;
 
 });
--- www/js/room/room.view.controller.js
+++ www/js/room/room.view.controller.js
@@ -1,9 +1,116 @@
 'use strict';
 
-app.factory('RoomViewCtrl', function () {
+app.service('RoomViewCtrl', function (Session, FbChatService, FbGamesService) {
 
     let $scope;
 
+    function _remove (playerArray, player) {
+        let idx = _.findIndex(playerArray, { _id: player._id });
+        idx > -1 ? playerArray.splice(idx, 1) : null;
+    }
 
+    function _has (playerArray, player) {
+        return _.findIndex(playerArray, { _id: player._id }) > -1;
+    }
+
+    function _notFirstTeamProposalOfQuest () {
+        return $scope.game.currentGamePhase === 'team building' &&
+            $scope.game.currentQuestIdx !== 0 &&
+            $scope.game.currentVoteTrack !== 0;
+    }
+
+    function _addToTeam (player) {
+        FbGamesService.addToTeam($scope.game.$id, player);
+    }
+
+    function _phaseIs (phase) {
+        if (phase === 'end') return $scope.game.currentGamePhase.slice(0, 3) === phase;
+        return $scope.game.currentGamePhase === phase;
+    }
+
+    this.registerScope = (scope) => $scope = scope;
+
+    this.closeGameStartModal = () => {
+        $scope.gameStartModal
+            .hide()
+            .then(() => $scope.gameStartModal.remove());
+    };
+    this.closeLadyModal = () => {
+        $scope.ladyModal
+            .hide()
+            .then(() => $scope.ladyModal.remove());
+    };
+    this.closeQuestResultModal = () => {
+        $scope.questResultModal
+            .hide()
+            .then(() => $scope.questResultModal.remove());
+    };
+    this.addMessage = (message) => message.text ? FbChatService.addChat($scope.chats, Session.user, message.text) : null;
+    this.isHost = () => Session.user._id === $scope.game.host;
+    this.ableToBegin = () => Object.keys($scope.game.players).length >= $scope.game.targetSize && Object.keys($scope.game.players).length < 11;
+    this.startGame = () => FbGamesService.startGame($scope.game);
+    this.me = (player) => player._id === $scope.user._id;
+    this.proposeTeam = () => {
+        return Promise.all($scope.selected.map(playerId => _addToTeam(playerId)))
+            .then(() => {
+                $scope.selected = [];
+                FbGamesService.proposeTeam($scope.game.$id);
+            });
+    };
+    this.disablePropose = () => $scope.selected.length !== $scope.game.currentQuestPlayersNeeded;
+    this.resetTeam = () => {
+        $scope.selected = [];
+        FbGamesService.resetTeam($scope.game.$id)
+    };
+    this.voteApprove = () => FbGamesService.approveTeam($scope.game.$id, $scope.userRecord.playerKey);
+    this.voteReject = () => FbGamesService.rejectTeam($scope.game.$id, $scope.userRecord.playerKey);
+    this.successQuest = () => FbGamesService.voteToSucceed($scope.game.$id, $scope.userRecord.playerKey);
+    this.failQuest = () => FbGamesService.voteToFail($scope.game.$id, $scope.userRecord.playerKey);
+    this.guessMerlin = () => {
+        let _player = $scope.selected[0];
+        $scope.selected = [];
+        FbGamesService.guessMerlin($scope.game.$id, _player);
+    };
+    this.range = (n, m) => _.range(n, m);
+    this.disableLady = () => {
+        if ($scope.selected.length !== 1) return true;
+        if ($scope.selected.length === 1) {
+            let _player = $scope.selected[0];
+            if (_player.hasBeenLadyOfTheLake || _player._id === user._id) return true;
+        }
+        return false;
+    };
+    this.disableGuessMerlin = () => {
+        if ($scope.selected.length !== 1) return true;
+        if ($scope.selected.length === 1) {
+            let _player = $scope.selected[0];
+            if (_player.loyalty !== 'good' || (_player.loyalty === 'evil' && _player.character !== 'Oberon')) return true;
+        }
+        return false;
+    };
+    this.useLady = () => {
+        let _player = $scope.selected[0];
+        $scope.selected = [];
+        $scope.investigatedPlayer = {
+            loyalty: _player.loyalty,
+            displayName: _player.displayName
+        }
+        FbGamesService.useLady($scope.game.$id, _player, $scope);
+    };
+    this.isSelected = (player) => _has($scope.selected, player);
+    this.select = (player) => $scope.isSelected(player) ? _remove($scope.selected, player) : $scope.selected.push(player);
+    this.revealPicture = (player) => {
+        if ($scope.game.waitingToPlay) return player.picture;
+        if (player.onQuest && _phaseIs('team voting')) return '/img/sigil.png';
+        if (player.approvedQuest && (_phaseIs('quest voting') || _notFirstTeamProposalOfQuest())) return '/img/approve.png';
+        if (!player.approvedQuest && _phaseIs('quest voting') || _notFirstTeamProposalOfQuest()) return '/img/reject.png';
+        if ($scope.game.useLady && $scope.game.currentLadyOfTheLake._id === player._id && !_phaseIs('end')) return '/img/lady_of_the_lake.png';
+        if ($scope.me(player) ||
+            (_phaseIs('end')) ||
+            (_phaseIs('guess merlin') &&
+                player.loyalty === 'evil' &&
+                player.character !== 'Oberon')) return player.imageUrl;
+        return player.picture;
+    }
 
 });
